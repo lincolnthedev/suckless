1634a66f75bbb908bfc74fd646a3c08ad602c533
lincolnthedev <lincolnthedev@askthetech.guru>
2021-07-06 15:04:09 -0400

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
-cd dwm
+printf "\n -> Copying confs"
+sleep 1
+cp confs/dwm/config.h suckless/dwm
+cp confs/st/config.h suckless/st
+cp confs/slstatus/config.h suckless/slstatus
+
+printf "\n -> Building dwm"
+sleep 1
+cd suckless/dwm
 make clean install
+
+printf "\n -> Building st"
+sleep 1
 cd ../st
 make clean install
+
+printf "\n -> Building slstatus"
+sleep 1
 cd ../slstatus
+make clean install
+
+clear
+printf "\nDone!\n\n"
